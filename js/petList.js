500bfb59cfbf17f3e5c4e54c8ae86d8c05fe587a
xuanqi35 <xuanqi357@gmail.com>
2024-05-01 17:13:44 +0800

    新增 css hover

---
+++ js/petList.js
@@ -0,0 +1,13 @@
+// document.addEventListener("state", function () {
+//   let pause = document.getElementById("state-pause");
+
+//   //滑鼠移入時改變背景顏色（藍）
+//   pause.addEventListener("mouseover", function () {
+//     pause.style.opacity = "1";
+//   });
+
+//   //滑鼠移出時恢復背景顏色
+//   pause.addEventListener("mouseout", function () {
+//     pause.style.opacity = "0";
+//   });
+// });
