9a52c0a46bcff22c5d8e51097bf1ba4b23cbae9d
xuanqi35 <xuanqi357@gmail.com>
2024-05-06 15:38:33 +0800

    修改 img

--- js/petInfo.js
+++ js/petInfo.js
@@ -7,16 +7,32 @@ const img4Ele = document.getElementById("img4");
 
 img1Ele.addEventListener("click", () => {
   imgViewEle.src = img1Ele.src;
+  img1Ele.classList.add("img-click");
+  img2Ele.classList.remove("img-click");
+  img3Ele.classList.remove("img-click");
+  img4Ele.classList.remove("img-click");
 });
 
 img2Ele.addEventListener("click", () => {
   imgViewEle.src = img2Ele.src;
+  img1Ele.classList.remove("img-click");
+  img2Ele.classList.add("img-click");
+  img3Ele.classList.remove("img-click");
+  img4Ele.classList.remove("img-click");
 });
 
 img3Ele.addEventListener("click", () => {
   imgViewEle.src = img3Ele.src;
+  img1Ele.classList.remove("img-click");
+  img2Ele.classList.remove("img-click");
+  img3Ele.classList.add("img-click");
+  img4Ele.classList.remove("img-click");
 });
 
 img4Ele.addEventListener("click", () => {
   imgViewEle.src = img4Ele.src;
+  img1Ele.classList.remove("img-click");
+  img2Ele.classList.remove("img-click");
+  img3Ele.classList.remove("img-click");
+  img4Ele.classList.add("img-click");
 });
