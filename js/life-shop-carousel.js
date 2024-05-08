38304bf6fb52a0a80e8cfefbc95222aeee115e28
paul <a0921675081@gmail.com>
2024-05-09 00:41:37 +0800

    商品輪播+顯示搜尋結果

---
+++ js/life-shop-carousel.js
@@ -0,0 +1,20 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 6,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+});
+var swiper = new Swiper(".mySwiper2", {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+});
